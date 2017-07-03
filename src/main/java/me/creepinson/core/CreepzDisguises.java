9598486972ec3eb8ebb29b774b0fc429f5705cb1
Aeronica <pmboese@gmail.com>
2017-07-03 12:00:46 -0500

    Cleanup warnings in main mod file.

--- src/main/java/me/creepinson/core/CreepzDisguises.java
+++ src/main/java/me/creepinson/core/CreepzDisguises.java
@@ -1,4 +1,5 @@
 package me.creepinson.core;
+
 import java.util.Random;
 import java.util.concurrent.Callable;
 
@@ -10,11 +11,16 @@ import me.creepinson.lib.proxy.CommonProxy;
 import me.creepinson.lib.util.Utils;
 import me.creepinson.lib.util.config.ConfigEventHandler;
 import me.creepinson.lib.util.config.ConfigUtils;
-import me.creepinson.packet.*;
+import me.creepinson.packet.CreepPacket;
+import me.creepinson.packet.DisguisePacket;
+import me.creepinson.packet.DisguisePacketHandler;
+import me.creepinson.packet.EnderPacket;
+import me.creepinson.packet.PacketHandler;
+import me.creepinson.packet.PacketHandler2;
+import net.minecraft.entity.Entity;
 import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.capabilities.CapabilityManager;
-import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.SidedProxy;
@@ -25,33 +31,34 @@ import net.minecraftforge.fml.common.network.NetworkRegistry;
 import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
 import net.minecraftforge.fml.common.registry.EntityRegistry;
 import net.minecraftforge.fml.relauncher.Side;
+
 @Mod(modid = Utils.MODID, version = Utils.VERSION)
 public class CreepzDisguises
 {
-	 
-	public static final SimpleNetworkWrapper INSTANCE = NetworkRegistry.INSTANCE.newSimpleChannel("mechannel");
 
-	@SidedProxy(serverSide = Utils.SEVERPROXY, clientSide = Utils.CLIENTPROXY)
-public static CommonProxy proxy;
+    public static final SimpleNetworkWrapper INSTANCE = NetworkRegistry.INSTANCE.newSimpleChannel("mechannel");
 
-public int movingBlockID;
-@Mod.Instance(Utils.MODID)
-public static CreepzDisguises instance;
- 
-    
+    @SidedProxy(serverSide = Utils.SEVERPROXY, clientSide = Utils.CLIENTPROXY)
+    public static CommonProxy proxy;
+
+    public int movingBlockID;
+
+    @Mod.Instance(Utils.MODID)
+    public static CreepzDisguises instance;
+
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
-    	int entityID = MathHelper.getRandomUUID().hashCode();
-    	movingBlockID = entityID;
+        int entityID = MathHelper.getRandomUUID().hashCode();
+        movingBlockID = entityID;
 
-     	Utils.getLogger().info("Pre Init");
+        Utils.getLogger().info("Pre Init");
 
         ConfigUtils.preInit();
 
-    	proxy.preInit();
-    	proxy.registerRenderers();
-    
+        proxy.preInit();
+        proxy.registerRenderers();
+
     }
 
     @EventHandler
@@ -60,55 +67,52 @@ public static CreepzDisguises instance;
         INSTANCE.registerMessage(PacketHandler2.class, CreepPacket.class, 0, Side.SERVER);
         INSTANCE.registerMessage(PacketHandler.class, EnderPacket.class, 1, Side.SERVER);
         INSTANCE.registerMessage(DisguisePacketHandler.class, DisguisePacket.class, 2, Side.SERVER);
-        CapabilityManager.INSTANCE.register(IDisguise.class, new DisguiseStorage(),  Disguise.class);
-    	Utils.getLogger().info("Init");
+        CapabilityManager.INSTANCE.register(IDisguise.class, new DisguiseStorage(), Disguise.class);
+        Utils.getLogger().info("Init");
         MinecraftForge.EVENT_BUS.register(new ConfigEventHandler());
-    	MinecraftForge.EVENT_BUS.register(new me.creepinson.handler.event.EventHandler());
-    	FMLCommonHandler.instance().bus().register(new KeysHandler());
-    	proxy.init();
+        MinecraftForge.EVENT_BUS.register(new me.creepinson.handler.event.EventHandler());
+        MinecraftForge.EVENT_BUS.register(new KeysHandler());
+        proxy.init();
 
     }
 
-public static class DisguiseFactory implements Callable<Disguise> 
-{
-@Override
-public Disguise call() throws Exception 
-{
-	return new Disguise();
-}
-}
+    public static class DisguiseFactory implements Callable<Disguise>
+    {
+        @Override
+        public Disguise call() throws Exception
+        {
+            return new Disguise();
+        }
+    }
 
     @EventHandler
     public void postInit(FMLPostInitializationEvent event)
     {
-      
-     	Utils.getLogger().info("Post Init");
-    	
-    	proxy.postInit();
-    	
+        Utils.getLogger().info("Post Init");
+        proxy.postInit();
     }
 
-    public static void registerEntity(Class entityClass, String name, int ID, int color1, int color2){
-        
-        
-    long seed = name.hashCode();
-    Random rand = new Random(seed);
-    int primaryColor = rand.nextInt() * 16777215;
-    int secondaryColor = rand.nextInt() * 16777215;
-
-    EntityRegistry.registerModEntity(entityClass, name, ID, instance, 64, 10, true, color1, color2);
+    @SuppressWarnings("unchecked")
+    public static void registerEntity(Class<?> entityClass, String name, int ID, int color1, int color2)
+    {
+        EntityRegistry.registerModEntity((Class<? extends Entity>) entityClass, name, ID, instance, 64, 10, true, color1, color2);
     }
-    public static void registerEntityNoEgg(Class entityClass, String name, int ID){
-        
-        
+
+    @SuppressWarnings("unchecked")
+    public static void registerEntity(Class<?> entityClass, String name, int ID)
+    {
         long seed = name.hashCode();
         Random rand = new Random(seed);
         int primaryColor = rand.nextInt() * 16777215;
         int secondaryColor = rand.nextInt() * 16777215;
 
-        EntityRegistry.registerModEntity(entityClass, name, ID, instance, 64, 10, true);
+        EntityRegistry.registerModEntity((Class<? extends Entity>) entityClass, name, ID, instance, 64, 10, true, primaryColor, secondaryColor);
+    }
+    
+    @SuppressWarnings("unchecked")
+    public static void registerEntityNoEgg(Class<?> entityClass, String name, int ID)
+    {
+        EntityRegistry.registerModEntity((Class<? extends Entity>) entityClass, name, ID, instance, 64, 10, true);
+    }
 
-	}
-    
-    
 }
